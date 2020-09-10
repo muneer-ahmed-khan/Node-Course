c3718c46c4c6cf82f561fdd6b613959d7c3a8b12
muneer khan <muneerkhan31886@gmail.com>
2020-09-10 11:32:48 +0500

    my first node commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const hbs = require('hbs')
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials/')
+app.set('view engine', 'hbs')
+// middleware
+app.use(express.static(__dirname + '/public'))
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  console.log(now)
+  next();
+})
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+})
+
+hbs.registerHelper('screamIt', (text) => {
+  // console.log(text)
+  return text
+})
+
+app.get('/', (req, res) => {
+  // res.send('hello express')
+  res.render('home.hbs',{
+    pageTitle:'Home Page'
+  })
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs',{
+    pageTitle:'About Page',
+    currentYear: new Date().getFullYear()
+  })
+});
+
+app.listen(3000, () => {
+  console.log('server is runing')
+});
